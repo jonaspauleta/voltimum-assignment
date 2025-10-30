557e0fcef93467d32985e0e75f0df48e4182aad6
João Paulo Santos <jonaspauleta2@gmail.com>
2025-10-30 13:20:37 +0000

    user factory and seeder update for is_admin field, .env.example updates

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,7 @@ final class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
+            'is_admin' => false,
             'two_factor_secret' => Str::random(10),
             'two_factor_recovery_codes' => Str::random(10),
             'two_factor_confirmed_at' => now(),
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,12 @@ final class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()
+            ->withoutTwoFactor()
+            ->create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+                'is_admin' => true,
+            ]);
     }
 }
